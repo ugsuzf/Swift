814b4fe98d8b42e06bf165f1906605dd88390144
Furkan Kaan Ugsuz <53346136+ugsuzf@users.noreply.github.com>
2020-08-28 16:43:05 +0200

    Add files via upload

---
+++ CryptoCurrencyMVVM/CryptoCrazy/Model/CryptoCurrency.swift
@@ -0,0 +1,14 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCrazy
+//
+//  Created by Furkan Kaan Ugsuz on 28.08.2020.
+//  Copyright © 2020 Furkan Kaan Ugsuz. All rights reserved.
+//
+
+import Foundation
+
+struct CryptoCurrency: Decodable {
+    let currency: String
+    let price: String
+}
---
+++ CryptoCurrencyMVVM/CryptoCrazy/Service/Webservice.swift
@@ -0,0 +1,36 @@
+//
+//  Webservice.swift
+//  CryptoCrazy
+//
+//  Created by Furkan Kaan Ugsuz on 28.08.2020.
+//  Copyright © 2020 Furkan Kaan Ugsuz. All rights reserved.
+//
+
+import Foundation
+
+class Webservice {
+    
+    func downloadCurrencies(url: URL, completion: @escaping ([CryptoCurrency]?) -> ()) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                
+                
+                let crytpoList = try? JSONDecoder().decode([CryptoCurrency].self, from: data)
+                print(crytpoList)
+                
+                if let cryptoList = crytpoList {
+                    completion(crytpoList)
+                }
+                
+            }
+            
+        }.resume()
+        
+    }
+    
+}
