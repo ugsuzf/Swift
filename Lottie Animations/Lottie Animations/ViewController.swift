2dec2b66692e8c4b7048eb000e4f5477412252f5
Furkan Kaan ugsuz <ugsuzf@gmail.com>
2020-11-11 20:23:53 +0100

    project created

---
+++ Lottie Animations/Lottie Animations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lottie Animations
+//
+//  Created by Furkan Kaan Ugsuz on 11/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
