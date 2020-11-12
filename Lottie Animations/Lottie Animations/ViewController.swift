0bff07365ac5cb7a4b0ae879b8f2d606f22d07d0
Furkan Kaan ugsuz <ugsuzf@gmail.com>
2020-11-12 13:20:21 +0100

    sample gif added

--- Lottie Animations/Lottie Animations/ViewController.swift
+++ Lottie Animations/Lottie Animations/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    var animationView: AnimationView?
+    override func viewDidLoad() { 
         // Do any additional setup after loading the view.
+        animationView = .init(name: "rocket")
+        animationView?.frame = view.bounds
+        animationView?.loopMode = .loop
+        view.addSubview(animationView!)
+        animationView?.play()
+        
+        
     }
 
 
